d1753f8b45556d30355e6ae87d4c62bbd2b4ef1d
Ravi Kumar <ravi.kc12@gmail.com>
2019-05-04 18:07:56 +0530

    first automapper use.

--- ApplicationCore/Services/UserService.cs
+++ ApplicationCore/Services/UserService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Conduit.ApplicationCore.DTOs;
 using Conduit.ApplicationCore.Entities;
 using Conduit.ApplicationCore.Interfaces.Account;
@@ -12,14 +13,17 @@ namespace Conduit.ApplicationCore.Services
     public class UserService : IUserService
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
 
-        public UserService(UserManager<ApplicationUser> userManager)
+        public UserService(UserManager<ApplicationUser> userManager, IMapper mapper)
         {
             _userManager = userManager;
+            _mapper = mapper;
         }
 
         public async Task<IdentityResult> RegisterAsync(UserRegistrationDto userRegistrationDto)
         {
+            var user = _mapper.Map<ApplicationUser>(userRegistrationDto);
             var result = await _userManager.CreateAsync(new ApplicationUser
             { Email = userRegistrationDto.Email, UserName = userRegistrationDto.UserName },
                 userRegistrationDto.Password);
---
+++ Infrastructure/Mappings/UserRegistrationProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Conduit.ApplicationCore.DTOs;
+using Conduit.ApplicationCore.Entities;
+
+namespace Conduit.Infrastructure.Mappings
+{
+    public class UserRegistrationProfile : BaseMapper
+    {
+        public UserRegistrationProfile()
+        {
+            CreateMap<UserRegistrationDto, ApplicationUser>()
+                .ReverseMap();
+        }
+    }
+}
